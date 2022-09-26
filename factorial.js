d1d8b0d3f4f546332025b004dceb3f1cdfb00d5a
TrudyCyns <gcyn51a@gmail.com>
2022-09-26 18:46:36 +0300

    BDD Demo Start

--- factorial.js
+++
@@ -1,9 +0,0 @@
-// Nabasirye Gertrude, Ssozi Martin Rukundo.
-
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-plusplus */
-module.exports = function factorial(num) {
-  if (num < 0) return "Can't find factorial for negative numbers";
-  if (num <= 1) return 1;
-  return num * factorial(num - 1);
-};
